020f99205762b865dbdd4a1ebc78c20e28429f5f
Lucas Bonomi <lucas.bonomi@gmail.com>
2016-10-29 19:53:45 +0200

    Update UI

--- build/javascripts/all.js
+++ build/javascripts/all.js
@@ -1,4 +1,4 @@
-// This is where it all goes :)
+ // This is where it all goes :)
 
 function loadJSON(callback) {
 	var xobj = new XMLHttpRequest();
@@ -14,8 +14,16 @@ function loadJSON(callback) {
 
 function init() {
 	var list  = [];
-	var template = [];
+	var desktopTemplate = [];
+	var mobileTemplate = [];
 	var target = document.querySelector('[data-inject-wallpapers]');
+	var wpDesktop = document.createElement('div');
+	var wpMobile = document.createElement('div');
+
+	wpDesktop.classList.add('desktop-wallpapers');
+	wpMobile.classList.add('mobile-wallpapers');
+	wpDesktop.setAttribute('colorify-gradient-color','')
+
 	var i = 0;
 
 	loadJSON(function(response) {
@@ -23,27 +31,85 @@ function init() {
 		actual_JSON.forEach(function(e){
 			list.push({
 				name: e.name,
-				url: e.url
+				url: e.url,
+				type: e.type
 			})
 		})
 
 		list.forEach(function(t) {
 			i++;
+			var dImage = document.createElement('img');
+			dImage.src = t.url;
+
+			var imgCont = document.createElement('div').innerHTML = dImage;
+
+			function nodeToString ( node ) {
+			   var tmpNode = document.createElement( "div" );
+			   tmpNode.appendChild( node.cloneNode( true ) );
+			   var str = tmpNode.innerHTML;
+			   tmpNode = node = null; // prevent memory leaks in IE
+			   return str;
+			}
+
 			var tpl = `
 				<div id="wp${i}" class="wallpaper">
-					<figure>
-						<img src="${t.url}" alt="${t.name}"/>
-						<figcaption>${t.name}</figcaption>
-					</figure>
-					<a href="${t.url}" download alt="Download ${t.name}.jpg" title="Download ${t.name}.jpg">Download</a>
+					<div>
+						<figure class="wallpaper__image">
+							${nodeToString(imgCont)}
+							<figcaption>${t.name}</figcaption>
+							<a href="${t.url}" class="button fab" download alt="Download ${t.name}.jpg" title="Download ${t.name}.jpg"></a>
+						</figure>
+						<div class="wallpaper__title">
+							<h3>${t.name}</h3>
+							<p>${t.type}</p>
+						</div>
+					</div>
 				</div>
-			`
-			template.push(tpl)
+			`;
+
+
+			if ( t.type === 'desktop' ) {
+				desktopTemplate.push(tpl)
+			} else {
+				mobileTemplate.push(tpl)
+			}
+
 		});
-		target.innerHTML = String(template).replace(/[,]/g,"");
+		wpDesktop.innerHTML = String(desktopTemplate).replace(/[,]/g,"");
+		wpMobile.innerHTML = String(mobileTemplate).replace(/[,]/g,"");
+		wpDesktop.classList.add('display');
+		target.appendChild(wpDesktop);
+		target.appendChild(wpMobile);
+
 	});
 }
 
-window.onload = function() {
-	init()
+
+
+function togglePanels() {
+	var hidden = document.querySelector('.desktop-wallpapers');
+	var trigger = document.querySelectorAll('.controls a');
+
+	for (var i = 0; i < trigger.length; i++) {
+		displayPanel(trigger[i]);
+	}
+
+	function displayPanel(element) {
+		element.addEventListener('click', function(){
+			var classEnd = this.hash.split(':')[1];
+			switch (classEnd) {
+			  case 'desktop':
+			    document.querySelector('.'+classEnd + '-wallpapers').classList.add('display');
+			    document.querySelector('.mobile-wallpapers').classList.remove('display');
+			    break;
+
+			  case 'mobile':
+			    document.querySelector('.'+classEnd + '-wallpapers').classList.add('display');
+			    document.querySelector('.desktop-wallpapers').classList.remove('display');
+			    break;
+			}
+
+		})
+	}
 }
+
